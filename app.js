d363550b77fff35e8c9e0459d73ad3e5cb4e9bcb
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-27 10:14:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require("express");
+//Server Variables
 
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4444;
 
+//Using js and css files
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-    res.send(index.html);
-});
+//setting view-engine
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//sending app to routes
+const htmlRoutes = require("./routes/htmlRoutes")(app);
+//const apiRoutes = require("./routes/apiRoutes")(app);
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port: ${PORT}`);
