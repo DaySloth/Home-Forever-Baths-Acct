d5f0528118c6f366529eb34fbd1f9469c4b6ac1d
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-27 23:44:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Server Variables
-
+const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4444;
@@ -12,9 +12,19 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//setting express-session
+const session = require("express-session");
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
 //sending app to routes
 const htmlRoutes = require("./routes/htmlRoutes")(app);
 //const apiRoutes = require("./routes/apiRoutes")(app);
+//const userRoutes = require("./routes/userRoutes")(app);
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port: ${PORT}`);
