439f446a27eaf87c56e6bdf133210acea1e6870d
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-29 18:33:24 -0600

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,13 @@ const auth = require("../users/auth.js");
 module.exports = (app)=> {
     app.get('/login', (req, res)=>{
         res.render("login");
-    })
+    });
 
     app.post('/user/login', (req, res) => {
         console.log(req.body);
+    });
+
+    app.get('/user/register/'+ process.env.REGISTER_KEY + '/:uuid', (req, res)=>{
+        res.render('register', {uuid: req.params.uuid})
     });
 }
