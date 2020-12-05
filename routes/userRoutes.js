8802842896586fcaf0479d550bcd97b390d982d8
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-05 02:31:15 -0600

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const auth = require("../users/auth.js");
+const shortid = require('shortid');
 
-module.exports = (app)=> {
-    app.get('/login', (req, res)=>{
+module.exports = (app) => {
+    app.get('/login', (req, res) => {
         res.render("login");
     });
 
@@ -9,11 +10,30 @@ module.exports = (app)=> {
         console.log(req.body);
     });
 
-    app.get('/user/register/'+ process.env.REGISTER_KEY + '/:uuid', (req, res)=>{
-        res.render('register', {uuid: req.params.uuid})
+    app.get('/user/register/' + process.env.REGISTER_KEY, (req, res) => {
+        const newUrl = '/user/register/' + process.env.REGISTER_KEY + '/' + shortid.generate();
+        res.redirect(newUrl);
+    })
+
+    app.get('/user/register/' + process.env.REGISTER_KEY + '/:uuid', (req, res) => {
+        res.render('register', { uuid: req.params.uuid })
     });
 
     app.post('/user/register', (req, res) => {
-        console.log(req.body);
+        auth.register(req.body)
+        .then(user=>{
+            const userObj = {
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email
+            };
+            
+            req.user = userObj;
+            res.sendStatus(200);
+        })
+        .catch(error=>{
+            res.status(500);
+            res.send("Email already in use");
+        });
     });
 }
