7e349a4ca427ba936213064b7df64047f54063b4
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-06 01:30:48 -0600

    Update auth.js

--- users/auth.js
+++ users/auth.js
@@ -5,35 +5,61 @@ const users = {
     register: function (userObj) {
         return new Promise((resolve, reject) => {
             userObj.password = bcrypt.hashSync(userObj.password, 10);
-            UserDb.findOne({email: userObj.email}, (err, existingUser)=>{
-                if(existingUser){
+            UserDb.findOne({ email: userObj.email }, (err, existingUser) => {
+                if (existingUser) {
 
-                reject("Email already in use");
+                    reject("Email already in use");
 
                 } else {
                     UserDb.create(userObj)
-                .then((data) => {
-                    resolve(data)
-                })
-                .catch((error) => {
-                    reject("Couldn't add user");
-                });
+                        .then((data) => {
+                            resolve(data)
+                        })
+                        .catch((error) => {
+                            reject("Couldn't add user");
+                        });
                 }
             })
-            
+
         })
 
     },
 
     login: function (userObj) {
-        console.log("login");
-        console.log(userObj);
-        //const comparePass = bcrypt.compareSync(userObj.password, dbPassword);
+        return new Promise((resolve, reject) => {
+            UserDb.findOne({ email: userObj.email }, (err, foundUser) => {
+                if (foundUser) {
+                    const comparePass = bcrypt.compareSync(userObj.password, foundUser.password);
+                    if (comparePass) {
+                        resolve(foundUser);
+                    } else {
+                        reject("Incorrect email or password");
+                    }
+                }else {
+                    reject("Incorrect email or password");
+                }
+
+            })
+
+        })
+
     },
 
     delete: function (userObj) {
-        console.log("delete");
-        console.log(userObj);
+        return new Promise((resolve, reject) => {
+            if(userObj.password === process.env.DELETE_KEY) {
+                UserDb.findOneAndDelete({ uuid: userObj.uuid }, (err, foundUser) => {
+                    if(foundUser){
+                        resolve(foundUser);
+                    }else{
+                        reject("Could not find user");
+                    }
+                });
+            }else {
+                reject("Incorrect password");
+            };
+            
+        });
     }
 
 };
