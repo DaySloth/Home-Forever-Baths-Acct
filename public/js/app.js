60f888c0603a5a96028b9cba03ec5897361d340b
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-06 22:54:42 -0600

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,113 @@
 //Main page
-$('#paidInFullBtn').click(function(e){  
-    e.preventDefault();    
-    $(this).toggleClass('active');
+$('#paidInFullBtn').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('paid');
+    if ($('#notPaidInFullBtn').hasClass('unpaid')) {
+        $('#notPaidInFullBtn').toggleClass('unpaid');
+        $('#paymentReceivedDiv').toggleClass('hide');
+    }
 });
 
+$('#notPaidInFullBtn').click(function (e) {
+    e.preventDefault();
+    if ($('#paidInFullBtn').hasClass('paid')) {
+        $('#paidInFullBtn').toggleClass('paid');
+    }
+    $(this).toggleClass('unpaid');
+    $('#paymentReceivedDiv').toggleClass('hide');
+});
 
+const fees = [];
 
+$('#addFeeBtn').click(function (e) {
+    e.preventDefault();
+    fees.push($('#feeInput').val());
+    $('#feeInput').val("");
+    loadFees();
+});
+
+function loadFees() {
+    $("#feesDiv").html("");
+    fees.forEach(el => {
+        $("#feesDiv").append(`<div class="item feeItem">$<span>${el}</span> <button class="ui icon button delFeeBtn"><i class="close icon"></i></button>`);
+    })
+}
+
+$(document).on('click', '.delFeeBtn', function (e) {
+    e.preventDefault();
+    let fee = $(this)[0].parentElement.children[0].innerText
+    if (fees.includes(fee)) {
+        fees.splice(fees.indexOf(fee), 1);
+        loadFees();
+    }
+});
+
+$('#installForm').on("submit", (event) => {
+    event.preventDefault();
+    let installPrice = parseInt($('#installPrice').val());
+    let specialOpts = [];
+    let submitFees = [];
+    let paymentStatus;
+    let paymentReceived;
+
+    if ($('#virtualOpt').is(":checked")) {
+        specialOpts.push("Virtual");
+    };
+    if ($('#digitalOpt').is(":checked")) {
+        specialOpts.push("Digital");
+    };
+    if ($('#hoaOpt').is(":checked")) {
+        specialOpts.push("HOA");
+    };
+
+    if (fees.length !== 0) {
+        fees.forEach(amount => {
+            submitFees.push(parseInt(amount));
+        });
+    };
+
+    if ($('#paidInFullBtn').hasClass('paid')) {
+        paymentStatus = "Paid";
+        paymentReceived = installPrice;
+    } else if ($('#notPaidInFullBtn').hasClass('unpaid')) {
+        paymentStatus = "Outstanding";
+        paymentReceived = parseInt($("#paymentReceivedInput").val());
+    }
+
+    function calcInstallAmount() {
+        let feeTotal = 0;
+        submitFees.forEach(fee=>{
+            feeTotal =+ fee
+        });
+        
+        return installPrice - feeTotal;
+    };
+
+    const installObj = {
+        install_date: $('#installDate').val(),
+        install_name: $('#installName').val(),
+        install_type: $('#installType').val(),
+        price: installPrice,
+        special_options: specialOpts,
+        salesman: $('#salesman').val(),
+        installer: $('#installerName').val(),
+        fees: submitFees,
+        payment_status: paymentStatus,
+        payment_received: paymentReceived,
+        outstanding_balance: (installPrice - paymentReceived),
+        payment_method: $('#paymentMethod').val(),
+        total_install_amount: calcInstallAmount(),
+        notes: $('#installNotes').val(),
+    };
+
+    $.post('/api/install', installObj)
+    .then(response=>{
+        console.log(response);
+    })
+    .catch(error=>{
+        console.log(error);
+    });
+});
 
 
 
@@ -86,13 +188,13 @@ $('#deleteForm').on("submit", (event) => {
         type: 'DELETE',
         data: deleteObj
     })
-    .then(response => {
-        $('#errorMessage').html("");
-        $('#successMessage').html(`Successfully deleted <span class='deletedName'>${response.first_name} ${response.last_name}<span> <br> <a href='/login'> return to login</a>`);
-    })
-    .catch((error) => {
-        $('#successMessage').html("");
-        $('#errorMessage').html(error.responseText);
-    });
-            
+        .then(response => {
+            $('#errorMessage').html("");
+            $('#successMessage').html(`Successfully deleted <span class='deletedName'>${response.first_name} ${response.last_name}<span> <br> <a href='/login'> return to login</a>`);
+        })
+        .catch((error) => {
+            $('#successMessage').html("");
+            $('#errorMessage').html(error.responseText);
+        });
+
 });
