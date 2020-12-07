820189351ccb375c11cfced598c4da8d11926978
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-06 22:54:44 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,14 @@
+const installDb = require('../models/install.js');
+
 module.exports = (app) => {
-    
+    app.post('/api/install', (req, res) => {
+        console.log(req.body);
+        installDb.create(req.body)
+        .then(data=>{
+            console.log(data);
+        })
+        .catch(error=>{
+            console.log(error);
+        });
+    });
 }
