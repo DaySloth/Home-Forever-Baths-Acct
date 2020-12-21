e26fc87dcf05b785ac4f2b2334328a410567d574
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-21 13:39:16 -0600

    Create navTabs.js

---
+++ hfbreact/src/components/NavTabs/navTabs.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useLocation } from "react-router-dom";
+
+function NavTabs(){
+    let path = useLocation().pathname;
+
+    return (
+        <div className="ui secondary pointing menu">
+            <a className={path === "/" ? "item active": "item" } href="/">
+                Home
+            </a>
+            <a className={path === "/outstanding" ? "item active": "item" } href="/outstanding">
+                Outstanding
+            </a>
+            <a className={path === "/history" ? "item active": "item" } href="/history">
+                History
+            </a>
+            <div className="right menu">
+                <a className="ui item" href="/logout">
+                    Logout
+                </a>
+            </div>
+        </div>
+    )
+};
+
+export default NavTabs;
