dcfdcea163b197d5167101234e80e42d977a7f49
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-22 15:37:24 -0600

    Update wrapper.js

--- hfbreact/src/components/Wrapper/wrapper.js
+++ hfbreact/src/components/Wrapper/wrapper.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InstallContext from "../../utils/context/installContext.js";
+import UserContext from "../../utils/context/userContext.js";
+import Auth from "../../utils/auth/auth.js";
 
 function Wrapper(props){
+
+    const [user, setUser] = useState({
+        loggedIn: false,
+        first_name: "",
+        last_name: "",
+        email: "",
+        errors: []
+    });
+
+    const [install, setInstall] = useState({
+        installDate: "",
+        installName: "",
+        installType: "",
+        price: 0,
+        specialOpts: [],
+        salesman: "",
+        installer: "",
+        fees: [],
+        paymentStatus: "",
+        paymentReceived: "",
+        paymentMethod: "",
+        notes: ""
+    });
+
+    function loginUser(userObj) {
+        console.log(userObj);
+    }
+
     return(
-        <div>{props.children}</div>
+
+        <InstallContext.Provider value={install}>
+            <UserContext.Provider value={{user, loginUser}}>
+                {props.children}
+            </UserContext.Provider>
+        </InstallContext.Provider>
+
     )
 };
 
