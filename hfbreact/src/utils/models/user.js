db5b55fd6caae8161be3b588f2deda3600b6fb07
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-22 15:37:43 -0600

    Create user.js

---
+++ hfbreact/src/utils/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    uuid: String,
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+    date_Updated: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
