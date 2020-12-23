6a2681b8757d342ccc442361d8bc552771bee7b2
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-22 18:27:02 -0600

    Update index.js

--- hfbreact/client/src/index.js
+++ hfbreact/client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import Wrapper from "./components/Wrapper/wrapper.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Wrapper>
+      <App />
+    </Wrapper>
   </React.StrictMode>,
   document.getElementById('root')
 );
