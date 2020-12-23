0cc737637027c067a930de16448ecc1bb5a0c2b1
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-23 12:35:42 -0600

    Update login.js

--- hfbreact/client/src/pages/login/login.js
+++ hfbreact/client/src/pages/login/login.js
@@ -1,39 +1,16 @@
 import React, { useContext, useState } from 'react';
-import UserContext from "../../utils/context/userContext.js";
+import {useAuth0} from '@auth0/auth0-react';
+import "./login.css";
 
 function Login(){
 
-    const { loginUser } = useContext(UserContext)
-
-    const [user, setUser] = useState({
-        email: "",
-        password: ""
-    })
-
-    function login(event){
-        event.preventDefault();
-        loginUser(user);
-    }
-
-    function handleInputChange(event) {
-        const {name, value} = event.target;
-        setUser({...user, [name]: value})
-    }
+    const {loginWithRedirect} = useAuth0();
 
     return (
-        <form className="ui form container" onSubmit={login}>
-        <div id="errorMessage"></div>
-        <div className="field">
-            <label>Email</label>
-            <input type="email" name="email" placeholder="your@email.com" onChange={handleInputChange} />
+        <div className=" container loginBtn">
+            <label>You must login to access this page</label>
+            <button className="ui black button large fluid" type="submit" onClick={()=>loginWithRedirect()}>Login</button>
         </div>
-        <div className="field">
-            <label>Password</label>
-            <input type="password" name="password" onChange={handleInputChange} />
-        </div>
-    
-        <button className="ui button" type="submit">Login</button>
-    </form>
     )
 };
 
