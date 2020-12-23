5b2fd7fb8386ae0e29ae612c66b3ae86ad926993
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-23 15:42:53 -0600

    Update install.js

--- hfbreact/routes/install/install.js
+++ hfbreact/routes/install/install.js
@@ -1,4 +1,28 @@
 const router = require("express").Router();
+const db = require("../../models");
 
+router.get("/api/install/outstanding", (req, res)=>{
+    db.Install.find({payment_status: "Outstanding"}, (err, data) => {
+        res.send(data);
+    });
+});
+
+router.get("/api/install", (req, res)=>{
+    db.Install.find({}, (err, data) => {
+        res.send(data);
+    });
+});
+
+router.post("/api/install", (req, res)=>{
+
+});
+
+router.put("/api/install/:id", (req, res)=>{
+
+});
+
+router.delete("/api/install/:id", (req, res)=>{
+
+});
 
 module.exports = router;
