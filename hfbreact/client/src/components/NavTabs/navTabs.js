66c59ab412f7f917bc62c10b5a71eb6da1e64c6c
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-23 12:35:33 -0600

    Update navTabs.js

--- hfbreact/client/src/components/NavTabs/navTabs.js
+++ hfbreact/client/src/components/NavTabs/navTabs.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
+import {useAuth0} from '@auth0/auth0-react';
 
 function NavTabs(){
+    const {user, logout} = useAuth0();
     let path = useLocation().pathname;
+    console.log(user);
 
     return (
         <div className="ui secondary pointing menu">
@@ -16,12 +19,14 @@ function NavTabs(){
                 History
             </a>
             <div className="right menu">
+                {user && (<div className="ui item">Welcome, {user.name}</div>)}
+                
                 {path === "/login" ?
                     <a className="ui item active" href="/logout">
                         Login
                     </a>
                     : 
-                    <a className="ui item" href="/logout">
+                    <a className="ui item" href="/logout" onClick={()=>logout()}>
                         Logout
                     </a> }
                 
