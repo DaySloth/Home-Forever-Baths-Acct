69fa63b224b3e70a30adc97db71c3f6bbf943e67
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-22 18:27:36 -0600

    Update server.js

--- hfbreact/server.js
+++ hfbreact/server.js
@@ -17,7 +17,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/homeForeverBath", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
 
 // Start the API server
 app.listen(PORT, function() {
