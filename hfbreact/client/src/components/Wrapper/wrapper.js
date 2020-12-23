5c6b37e3630e111ffe05297650cb205ed12740dd
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-23 12:35:35 -0600

    Update wrapper.js

--- hfbreact/client/src/components/Wrapper/wrapper.js
+++ hfbreact/client/src/components/Wrapper/wrapper.js
@@ -1,50 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import InstallContext from "../../utils/context/installContext.js";
-import UserContext from "../../utils/context/userContext.js";
-import API from "../../utils/api/API.js";
+//import API from "../../utils/api/API.js";
+import Home from "../../pages/home/home.js";
+import Outstanding from "../../pages/outstanding/outstanding.js";
+import History from "../../pages/history/history.js";
+import Login from "../../pages/login/login.js";
+import {useAuth0} from '@auth0/auth0-react';
 
-function Wrapper(props){
 
-    const [user, setUser] = useState({
-        loggedIn: true,
-        first_name: "",
-        last_name: "",
-        email: "",
-        errors: []
-    });
+function Wrapper() {
+  const {user, isAuthenticated} = useAuth0();
 
-    const [install, setInstall] = useState({
-        installDate: "",
-        installName: "",
-        installType: "",
-        price: 0,
-        specialOpts: [],
-        salesman: "",
-        installer: "",
-        fees: [],
-        paymentStatus: "",
-        paymentReceived: "",
-        paymentMethod: "",
-        notes: ""
-    });
+  const [install, setInstall] = useState({
+    installDate: "",
+    installName: "",
+    installType: "",
+    price: 0,
+    specialOpts: [],
+    salesman: "",
+    installer: "",
+    fees: [],
+    paymentStatus: "",
+    paymentReceived: "",
+    paymentMethod: "",
+    notes: "",
+  });
 
-    function loginUser(userObj) {
-        API.login(userObj)
-    };
 
-    function registerUser(userObj) {
-        API.register(userObj)
-    };
+  return (
+    <InstallContext.Provider value={install}>
+      
+        
 
-    return(
+        {isAuthenticated ? (
+          <>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <InstallContext.Provider value={install}>
-            <UserContext.Provider value={{user, loginUser}}>
-                {props.children}
-            </UserContext.Provider>
-        </InstallContext.Provider>
+          <Route exact path="/outstanding">
+            <Outstanding />
+          </Route>
 
-    )
-};
+          <Route exact path="/history">
+            <History />
+          </Route>
+          </>
+        ) :
+        <Login />
+        }
+
+      
+    </InstallContext.Provider>
+  );
+}
 
 export default Wrapper;
