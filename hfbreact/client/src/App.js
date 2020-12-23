c535994e024efde694122d39054812f0b420c0c0
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-22 18:26:58 -0600

    Update App.js

--- hfbreact/client/src/App.js
+++ hfbreact/client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs/navTabs.js";
-import Wrapper from "./components/Wrapper/wrapper.js";
 import Home from "./pages/home/home.js";
 import Outstanding from "./pages/outstanding/outstanding.js";
 import History from "./pages/history/history.js";
@@ -10,30 +9,26 @@ import UserContext from "./utils/context/userContext.js";
 
 function App() {
 
-  const { loggedIn } = useContext(UserContext)
-
+  const { user } = useContext(UserContext);
   return (
     <Router>
-        <Wrapper>
-
           <NavTabs />
 
           <Route exact path="/">
-            {loggedIn ? <Home /> : <Redirect to="/login" />}
+            {user.loggedIn ? <Home /> : <Redirect to="/login" />}
           </Route>
 
           <Route path="/outstanding">
-            {loggedIn ? <Outstanding /> : <Redirect to="/login" />}
+            {user.loggedIn ? <Outstanding /> : <Redirect to="/login" />}
           </Route>
 
           <Route path="/history">
-            {loggedIn ? <History /> : <Redirect to="/login" />}
+            {user.loggedIn ? <History /> : <Redirect to="/login" />}
           </Route>
 
           <Route path="/login">
             <Login />
           </Route>
-        </Wrapper>
     </Router>
   );
 }
